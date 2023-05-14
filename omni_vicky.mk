c57b88f093fe11e61bab39fe7d327e5d37f73aa1
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2023-05-14 07:31:47 +0200

    vicky: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: motorola Model: moto g72 Script version: 3.0.0

---
+++ omni_vicky.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit from vicky device
+$(call inherit-product, device/motorola/vicky/device.mk)
+
+PRODUCT_DEVICE := vicky
+PRODUCT_NAME := omni_vicky
+PRODUCT_BRAND := motorola
+PRODUCT_MODEL := moto g72
+PRODUCT_MANUFACTURER := motorola
+
+PRODUCT_GMS_CLIENTID_BASE := android-motorola
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="vicky_g_vext-user 12 S3SVS32.45-28-2-3 c4e712 release-keys"
+
+BUILD_FINGERPRINT := motorola/vicky_g_vext/vicky:12/S3SVS32.45-28-2-3/c4e712:user/release-keys
