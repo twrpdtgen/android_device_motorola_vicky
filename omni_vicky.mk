aa7830e493d8ca339c1a1c633ab9f49601af8e7d
Atharva Pandey <finitecode.contact@gmail.com>
2023-07-30 09:48:58 +0530

    Update omni_vicky.mk

--- omni_vicky.mk
+++ omni_vicky.mk
@@ -10,13 +10,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/lineage/config/common.mk)
 
 # Inherit from vicky device
 $(call inherit-product, device/motorola/vicky/device.mk)
 
 PRODUCT_DEVICE := vicky
-PRODUCT_NAME := omni_vicky
+PRODUCT_NAME := lineage_vicky
 PRODUCT_BRAND := motorola
 PRODUCT_MODEL := moto g72
 PRODUCT_MANUFACTURER := motorola
